3e0adbc22761e8731dd268b5a4d01f8c79400c44
Muush <2419108998@qq.com>
2025-08-05 00:17:48 +0800

    fix bug

--- run_fusion.py
+++ run_fusion.py
@@ -164,7 +164,7 @@ def run_macro_fusion(args, layers, inps, attention_mask, position_ids, dev):
         inps_run = compute_inps_run(layers, inps, attention_mask, position_ids, layer_start_idx = group[0], args = args)
 
         group_importance = [i for i in importance_list if i in group and i not in dangerous_list]
-        importance_rank = [group_importance.index(i) / (len(group_importance) - 1) for i in group]
+        importance_rank = [group_importance.index(i) / (len(group_importance) - 1) if i not in dangerous_list else -1 for i in group]
         Fuse_manager = Fuser(layers_2fuse, inps_run, attention_mask, position_ids, fuse_idx_t, target_idx_t, importance_rank, args)
 
         Fuse_manager.compute_full_states()
